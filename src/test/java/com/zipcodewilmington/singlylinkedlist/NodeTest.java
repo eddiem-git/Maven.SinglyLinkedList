b1eb1d65ce433b7a913fa5f3063d4c6123ea66f6
eddie <eddiem9657@gmail.com>
2019-11-30 14:23:11 -0500

    Singly Linked List

---
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/NodeTest.java
@@ -0,0 +1,34 @@
+package com.zipcodewilmington.singlylinkedlist;
+
+import jdk.nashorn.internal.ir.Node;
+import org.junit.Test;
+
+public class NodeTest {
+
+    @Test
+    public void testGetData(){
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void testSetNextNode(){
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void testGetNextNode(){
+        //given
+
+        //when
+
+        //then
+    }
+}
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,66 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import jdk.internal.jline.internal.TestAccessible;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void testDecrement(){
+        //given
+        SinglyLinkedList sLL = new SinglyLinkedList();
+        Integer expected = null;
+        //when
+        sLL.decrementCounter();
+        Integer actual = null;
+        //then
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void testIncrement(){
+        //given
+        SinglyLinkedList sLL = new SinglyLinkedList();
+        Integer expected = null;
+        //when
+        sLL.incrementCounter();
+        Integer actual = null;
+        //then
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void testAdd(){
+        //given
+        SinglyLinkedList sLL = new SinglyLinkedList();
+
+        //when
+        sLL.add(null);
+        //then
+
+    }
+
+    @Test
+    public void testGetCounter(){
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void testToString(){
+        //given
+
+        //when
+
+        //then
+    }
 }
